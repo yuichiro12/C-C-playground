1be913647539ba2ef661caa254c652ec09d0fc94
yuichiro12 <talcnrhyme@gmail.com>
2016-05-11 02:15:07 +0900

    ascii test program

---
+++ ascii.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void a()
+{
+  putchar(67);
+  putchar(97);
+  putchar(114);
+  putchar(112);
+  putchar(10);
+}
+
+int main()
+{
+  printf("%d %d %d %d %d %d %d %d\n", 'Y', 'U', 'i', 'c', 'H', 'i', 'r', 'o');
+  /* a(); */
+  return 0;
+}
